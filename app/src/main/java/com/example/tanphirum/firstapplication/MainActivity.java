aaa5d27abf464c02fc34494e333377382e0dc283
phirum <phirumtan@gmail.com>
2018-05-22 15:29:19 +0700

    add for loop

--- app/src/main/java/com/example/tanphirum/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/tanphirum/firstapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tanphirum.firstapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        for (int i=0; i < 10; i++)
+            Log.d("debug", "i =" + i);
+
     }
 
     public void sendMessage(View v) {
