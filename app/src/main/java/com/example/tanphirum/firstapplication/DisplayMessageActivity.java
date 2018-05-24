88f73fbbbef0ecb5b7ce0d101d2ffc1b74a3da4d
phirum <phirumtan@gmail.com>
2018-05-24 16:33:39 +0700

    start new activity

--- app/src/main/java/com/example/tanphirum/firstapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/tanphirum/firstapplication/DisplayMessageActivity.java
@@ -3,12 +3,39 @@ package com.example.tanphirum.firstapplication;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    private TextView mTxtMsg;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_msg);
+
+        mTxtMsg = findViewById(R.id.txtMsg);
+
+        if (getIntent().getExtras() != null) {
+            mTxtMsg.setText(getIntent().getExtras().getString(MainActivity.EXT_KEY_MSG));
+        } else {
+            mTxtMsg.setText("something wrong with bundle key");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/example/tanphirum/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/tanphirum/firstapplication/MainActivity.java
@@ -1,26 +1,52 @@
 package com.example.tanphirum.firstapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String EXT_KEY_MSG = "ext_key_msg";
+
+    private EditText mEdtMsg;
+    private Button mBtnSend;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEdtMsg = findViewById(R.id.edt_enter_msg);
 
-        for (int i=0; i < 10; i++)
-            Log.d("debug", "i =" + i);
+        mBtnSend = findViewById(R.id.btn_send);
+        mBtnSend.setOnClickListener(this);
 
     }
 
     public void sendMessage(View v) {
-        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra(EXT_KEY_MSG, mEdtMsg.getText().toString());
+        startActivity(intent);
     }
 
+    @Override
+    public void onClick(final View v) {
+        switch (v.getId()) {
+            case R.id.btn_send:
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        sendMessage(v);
+                    }
+                }, 3000);
+
+                break;
+        }
+    }
 }
